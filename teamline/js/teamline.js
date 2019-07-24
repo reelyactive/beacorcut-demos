c7b5b2380052848b67d127eef4e7fe0954138aa7
jeffyactive <jeff@reelyactive.com>
2019-07-24 12:16:16 -0400

    Completed year selection functionality

--- teamline/js/teamline.js
+++ teamline/js/teamline.js
@@ -55,21 +55,65 @@ const STORIES_BY_YEAR = {
 
 // DOM elements
 let years = document.querySelectorAll('.year');
+let forward = document.querySelector('#forward');
+let backward = document.querySelector('#backward');
 let cards = document.querySelector('#cards');
+
+
+// Local variables
+let currentYearId = LATEST_YEAR;
 
 
 // Update the year
 function handleYearSelection(event) {
-  let selectedYear = this;  // currentTarget of event
-  let selectedYearId = selectedYear.getAttribute('id');
+  let selectedId = this.getAttribute('id');
+  let selectedYearId = currentYearId;
+
+  if(selectedId === 'forward') {
+    selectedYearId = (parseInt(currentYearId) + 1).toString();
+  }
+  else if(selectedId === 'backward') {
+    selectedYearId = (parseInt(currentYearId) - 1).toString();
+  }
+  else {
+    selectedYearId = selectedId;
+  }
+
+  currentYearId = selectedYearId;
+  updateControls(selectedYearId);
 
   years.forEach(function(year) {
-    year.setAttribute('class', 'page-item year');
+    let isSelectedYear = (year.getAttribute('id') === selectedYearId);
+    if(isSelectedYear) {
+      year.setAttribute('class', 'page-item year active');
+    }
+    else {
+      year.setAttribute('class', 'page-item year');
+    }
   });
-  selectedYear.setAttribute('class', 'page-item year active');
 
   let selectedYearStoryUrls = STORIES_BY_YEAR[selectedYearId];
   updateCards(selectedYearStoryUrls);
+}
+
+
+// Update the forward and backward controls based on the given year
+function updateControls(selectedYearId) {
+  if(selectedYearId === EARLIEST_YEAR) {
+    backward.setAttribute('class', 'page-item disabled');
+  }
+  else {
+    backward.setAttribute('class', 'page-item');
+    backward.addEventListener('click', handleYearSelection, { once: true });
+  }
+
+  if(selectedYearId === LATEST_YEAR) {
+    forward.setAttribute('class', 'page-item disabled');
+  }
+  else {
+    forward.setAttribute('class', 'page-item');
+    forward.addEventListener('click', handleYearSelection, { once: true });
+  }
 }
 
 
@@ -104,5 +148,9 @@ years.forEach(function(year) {
 });
 
 
+// Observe backward click once
+backward.addEventListener('click', handleYearSelection, { once: true });
+
+
 // On page load, select the latest year
-updateCards(STORIES_BY_YEAR[LATEST_YEAR]);
+updateCards(STORIES_BY_YEAR[currentYearId]);
